2ed17f601b86eee11550d0082a868004c2b97c76
Rod Wilhelmy <rwilhelmy@gmail.com>
2014-05-08 23:53:02 -0500

    Replaces block for ampersand

--- models/object.rb
+++ models/object.rb
@@ -7,7 +7,7 @@ class Object
     elsif self.is_a?(Hash)
       self.each { |k,v| v.remove_extra_spacing }
     elsif self.is_a?(Array)
-      self.each { |e| e.remove_extra_spacing }
+      self.each(&:remove_extra_spacing)
     end
   end
 
